fd4a5ccab6d25f7b79d607633e37a4ae952c7049
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-15 19:48:38 +0530

    Added: Project files

---
+++ src/main/java/com/tdd/InvoiceGenerator.java
@@ -0,0 +1,13 @@
+package com.tdd;
+
+/**
+ * Invoice Generator
+ *
+ */
+public class InvoiceGenerator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to cab invoice generator!");
+    }
+}
