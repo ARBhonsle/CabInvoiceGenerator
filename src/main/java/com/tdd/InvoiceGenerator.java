4d018b41ba09c9a8662bb71bc1ab53b5d7a22b7a
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-15 20:37:52 +0530

    Added: Calculated aggregate total fare for multiple rides

--- src/main/java/com/tdd/InvoiceGenerator.java
+++ src/main/java/com/tdd/InvoiceGenerator.java
@@ -7,6 +7,7 @@ package com.tdd;
 public class InvoiceGenerator
 {
     private static final double RATE_PER_KM=10.0,RATE_PER_MIN=1.0;
+    private static final int MIN_RATE = 5;
 
     public static void main( String[] args )
     {
@@ -14,8 +15,15 @@ public class InvoiceGenerator
     }
 
     public double calculateFare(double distanceInKm, double timeInMin) {
-        int minRate = 5;
         double totalFare = RATE_PER_KM*distanceInKm+RATE_PER_MIN*timeInMin;
-        return minRate>totalFare?minRate:totalFare;
+        return MIN_RATE>totalFare?MIN_RATE:totalFare;
+    }
+
+    public double calculateAggregateFare(double[] distanceInKm, double[] timeInMin) {
+        double fare=0.0;
+        for (int i=0;i<distanceInKm.length;i++){
+            fare += calculateFare(distanceInKm[i],timeInMin[i]);
+        };
+        return fare;
     }
 }
--- src/test/java/com/tdd/InvoiceGeneratorTest.java
+++ src/test/java/com/tdd/InvoiceGeneratorTest.java
@@ -8,17 +8,22 @@ import org.junit.Test;
 /**
  * Unit test for simple InvoiceGenerator.
  */
-public class InvoiceGeneratorTest
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class InvoiceGeneratorTest {
+    InvoiceGenerator invoice = new InvoiceGenerator();
+
     @Test
-    public void givenDistanceAndTime_shouldReturnTotalFare()
-    {
-        InvoiceGenerator invoice = new InvoiceGenerator();
-        double distance=2.0, time=5.0;
-        double fare = invoice.calculateFare(distance,time);
-        Assert.assertTrue(fare==25);
+    public void givenDistanceAndTime_shouldReturnTotalFare() {
+        double distance = 2.0, time = 5.0;
+        double fare = invoice.calculateFare(distance, time);
+        Assert.assertTrue(fare == 25);
+    }
+    @Test
+    public void givenMultipleRides_shouldReturnAggregateTotalFare() {
+        double[] distance = {2.0, 3.0, 4.0};
+        double[] time = {5.0, 8.0, 10.0};
+        double totalAggregateFare=113;
+        double fare = invoice.calculateAggregateFare(distance,time);
+        Assert.assertEquals(totalAggregateFare,fare,0.0);
+        Assert.assertEquals(totalAggregateFare/3, fare/3,0.0);
     }
 }
